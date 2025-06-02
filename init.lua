711fd0ab08217e64f718f3516fe7c78efaaa87c8
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-06-02 12:13:12 +0200

    ws now opens warp window if there is only one window

--- init.lua
+++ init.lua
@@ -14,19 +14,25 @@ end)
 function sw()
 	local warpApp = hs.application.find("Warp")
 	if not warpApp then
-		--	print("warp is not running")
+		print("warp is not running")
 		return
 	end
 
 	local windows = warpApp:allWindows()
+
 	if #windows == 0 then
-		--	print("No warp windows found")
+		print("No warp windows found")
 		return
 	end
 
-	local found = false
-	--	print("Finestre trovate: " .. #windows)
+	if #windows == 1 then -- if there is only one window, focus on it
+		windows[1]:focus()
+		return
+	end
 
+	-- else switch among the windows
+
+	local found = false
 	local i = 0
 
 	for _, window in ipairs(windows) do
