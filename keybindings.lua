6e01310822388c73be0adb5396b07bdca8435040
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-09-14 13:06:45 +0200

    add custom keybinging

--- keybindings.lua
+++ keybindings.lua
@@ -1,15 +1,12 @@
--- Carica i file dalla cartella Spoons
 require("Spoons.cursor")
 require("Spoons.warp")
 
--- Carica e avvia il modulo temp-keybindings
-local tempKeybindings = require("Spoons.temp-keybindings")
+local tempKeybindings = require("Spoons.temp-keybindings") -- Carica e avvia il modulo temp-keybindings
 tempKeybindings:init()
 
--- Carica il modulo per Chrome hotkeys
-local chrome_hotkeys = require("Spoons.chrome_hotkeys")
+local chrome_hotkeys = require("Spoons.chrome_hotkeys") -- Carica il modulo per Chrome hotkeys
 
-hs.hotkey.bind({ "alt" }, "escape", function()
+hs.hotkey.bind({ "alt" }, "escape", function() -- apre cursor con option escape
 	local cmd = [[open -a "cursor"]]
 	hs.task
 		.new(
@@ -28,19 +25,20 @@ hs.hotkey.bind({ "alt" }, "escape", function()
 		:start()
 end)
 
--- Carica il modulo per riprendere pagine Chrome
-require("Spoons.resume_chrome_page")
-
--- Carica il modulo per le lettere greche
-require("Spoons.keys_substitutions")
-
--- Attiva il toggle dei devtools di Chrome con il tasto ù
-chrome_hotkeys.open_dev_tools_shortcut()
-
--- Attiva la funzione sw quando si preme command + spazio
+require("Spoons.resume_chrome_page") -- apre la pagina chrome con cmd y
+require("Spoons.keys_substitutions") -- Carica il modulo per le lettere greche
+chrome_hotkeys.open_dev_tools_shortcut() -- Attiva il toggle dei devtools di Chrome con il tasto ù
 hs.hotkey.bind({ "cmd" }, "escape", function()
+	-- switch between warp instances
+	-- Attiva la funzione sw quando si preme command + spazio
 	sw()
 end)
 
---  Φaggiungi che µ (equivalente a option m ) corrisponda alla sostituzione della stringa
---  "$()" con il cursore che si mette tra le parentesi
+-- # custom substitutions
+-- # bash
+hs.hotkey.bind({ "alt" }, "m", function()
+	-- Aggiunge sostituzione per µ (option+m): inserisce "$())" con cursore tra parentesi
+	hs.eventtap.keyStrokes([["$()"]]) -- literal: "$()"
+	hs.eventtap.keyStroke({}, "left")
+	hs.eventtap.keyStroke({}, "left")
+end)
